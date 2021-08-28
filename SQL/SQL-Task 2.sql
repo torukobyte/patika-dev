df50411491e1cbe79a5f7f1e428f8d91a1b4981a
Burak KALAYCI <torukobyte@gmail.com>
2021-08-29 01:48:20 +0300

    missing semicolons added

--- SQL/SQL-Task 2.sql
+++ SQL/SQL-Task 2.sql
@@ -2,16 +2,16 @@
 
 SELECT *
 FROM film
-WHERE replacement_cost BETWEEN 12.99 AND 16.98 -- 16.99 dan küçük dediği için 16.98 yazdım.
+WHERE replacement_cost BETWEEN 12.99 AND 16.98; -- 16.99 dan küçük dediği için 16.98 yazdım.
 
 /* actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.) */
 
 SELECT first_name,last_name
 FROM actor
-WHERE first_name IN ('Penelope','Nick','Ed')
+WHERE first_name IN ('Penelope','Nick','Ed');
 
 /* film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.) */
 
 SELECT *
 FROM film
-WHERE rental_rate IN (0.99,2.99,4.99) AND replacement_cost IN (12.99,15.99,28.99)
+WHERE rental_rate IN (0.99,2.99,4.99) AND replacement_cost IN (12.99,15.99,28.99);

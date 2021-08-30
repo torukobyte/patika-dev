654d96842450b372c06a82e11b1d5c2e6afb0f40
Burak KALAYCI <torukobyte@gmail.com>
2021-08-30 20:58:58 +0300

    task 7 updated

--- SQL/SQL-Task 7.sql
+++ SQL/SQL-Task 7.sql
@@ -1,8 +1,8 @@
 /* film tablosunda bulunan filmleri rating değerlerine göre gruplayınız. */
 
-SELECT rating,title
+SELECT rating,COUNT(*)
 FROM film
-GROUP BY rating,title
+GROUP BY rating
 ORDER BY rating;
 
 /* film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız. */
